2ac26b374ffb00382c2d68dbbd4848489ed98750
amolixs <kaliroot08@gmail.com>
2019-03-12 21:16:35 +0100

    ajout du label resultat

--- src/be/amolixs/frame/Frame.java
+++ src/be/amolixs/frame/Frame.java
@@ -1,6 +1,8 @@
 package be.amolixs.frame;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import be.amolixs.constante.ConstanteContainer;
 
@@ -24,18 +26,60 @@ public class Frame extends JFrame {
 	private ConstanteContainer constanteContainer;
 	
 	/**
+	 * Pannel principal.
+	 * @author amolixs
+	 */
+	private JPanel mainPannel;
+	
+	/**
+	 * Pannel button.
+	 * @author amolixs
+	 */
+	private JPanel pannelButton;
+	
+	/**
+	 * Label ou va etre affiché les resultat
+	 * @author amolixs
+	 */
+	private JLabel labelResult;
+	
+	/**
 	 * Constructeur.
 	 * @author amolixs
+	 * @param title
+	 * 		Titre de la fenetre
 	 */
 	public Frame(String title) {
 		// Instance object
 		this.constanteContainer = new ConstanteContainer();
-		// Config frame
+		init("*---CALCULATOR--*");
+		initComponent();
+		this.setVisible(true);
+	}
+	
+	/**
+	 * Méthode qui permet d'initialisé les composants de la fenetre
+	 * @author amolixs
+	 */
+	void initComponent() {
+		this.labelResult = new JLabel();
+		this.labelResult.setText("0");
+		this.mainPannel = new JPanel();
+		this.mainPannel.add(this.labelResult);
+		this.setContentPane(mainPannel);
+	}
+	
+	/**
+	 * Méthode qui permet d'initalisé la fenetre.
+	 * @author amolixs
+	 * @param title
+	 * 		Titre de la fenetre
+	 */
+	void init(String title) {
 		this.title = title;
 		this.setTitle(title);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.setSize(constanteContainer.width, constanteContainer.height);
-		this.setVisible(true);
 	}
 }
