979b11f24691ce39714077fdc387e56bf631596e
amolixs <kaliroot08@gmail.com>
2019-03-12 19:51:14 +0100

    initialisation

---
+++ src/be/amolixs/main/Main.java
@@ -0,0 +1,19 @@
+package be.amolixs.main;
+
+/**
+ * Fichier principal du project qui contient la class Main(classe principal)
+ * @version 1.0
+ * @author amolixs
+ */
+
+public class Main {
+
+	/**
+	 * Méthode principal du programme.
+	 * @author amolixs
+	 * @param args
+	 * 		Tableau contenant les arguments passé au programme.
+	 */
+	public static void main(String[] args) {
+	}
+}
