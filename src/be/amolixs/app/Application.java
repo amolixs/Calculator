2665c63132c90598c2d41fbb5d077c6e894ef054
amolixs <kaliroot08@gmail.com>
2019-03-12 20:32:15 +0100

    création des classes

---
+++ src/be/amolixs/app/Application.java
@@ -0,0 +1,24 @@
+package be.amolixs.app;
+
+/**
+ * Classe qui permet de gérer tous le programme
+ * @version 1.0
+ * @author amolixs
+ */
+
+public class Application {
+	
+	/**
+	 * Constructeur.
+	 * @author amolixs
+	 */
+	public Application() {
+	}
+	
+	/**
+	 * Méthode qui permet de lancer le programme.
+	 * @author amolixs
+	 */
+	public void run() {	
+	}
+}
---
+++ src/be/amolixs/frame/Frame.java
@@ -0,0 +1,19 @@
+package be.amolixs.frame;
+
+import javax.swing.JFrame;
+
+/**
+ * Classe qui permet de gérer la fenetre
+ * @version 1.0
+ * @author amolixs
+ */
+
+public class Frame extends JFrame {
+	
+	/**
+	 * Constructeur.
+	 * @author amolixs
+	 */
+	public Frame() {
+	}
+}
